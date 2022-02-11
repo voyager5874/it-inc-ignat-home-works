d4ef1f1eebd87197fd6b36f6f5fcaef914bcb398
Aleksandr Savkin <voyager5874@gmail.com>
2022-02-11 21:34:09 +0300

    refactor task11 - get rid of two useState hooks in the component, use parent component local state directly

--- src/p2-homeworks/h11/common/c8-SuperDoubleRange/SuperDoubleRange.tsx
+++ src/p2-homeworks/h11/common/c8-SuperDoubleRange/SuperDoubleRange.tsx
@@ -21,25 +21,20 @@ const SuperDoubleRange: React.FC<SuperDoubleRangePropsType> = (
 ) => {
     // сделать самому, можно подключать библиотеки
 
-
-    const [leftThumbPos, setLeftThumbPos] = useState(value[0])
-    const [rightThumbPos, setRightThumbPos] = useState(value[1])
-
     const getBarFillPercent = (leftThumbPos: number, rightThumbPos: number) => {
         const barFillLeftShift = Math.round(((leftThumbPos - min) / (max - min)) * 100)
         const barFillWidth = Math.round(((rightThumbPos - min) / (max - min)) * 100) - barFillLeftShift
         return [barFillLeftShift, barFillWidth]
     }
 
-    const [barFillState, setBarFillState] = useState(getBarFillPercent(leftThumbPos, rightThumbPos))
+    const [barFillState, setBarFillState] = useState(getBarFillPercent(value[0], value[1]))
 
     //make sure minVal does not exceed maxVal
     const leftThumbMoveHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
         const newLeftThumbPos = Number(event.currentTarget.value)
-        if (newLeftThumbPos < rightThumbPos) {
-            onChangeRange && onChangeRange([newLeftThumbPos, rightThumbPos])
-            setBarFillState(getBarFillPercent(newLeftThumbPos, rightThumbPos))
-            setLeftThumbPos(newLeftThumbPos)
+        if (newLeftThumbPos < value[1]) {
+            onChangeRange && onChangeRange([newLeftThumbPos, value[1]])
+            setBarFillState(getBarFillPercent(newLeftThumbPos, value[1]))
         }
         return
     }
@@ -47,16 +42,15 @@ const SuperDoubleRange: React.FC<SuperDoubleRangePropsType> = (
     //make sure maxVal does not fall below minVal
     const rightThumbMoveHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
         const newRightThumbValue = Number(event.currentTarget.value)
-        if (newRightThumbValue > leftThumbPos) {
-            onChangeRange && onChangeRange([leftThumbPos, newRightThumbValue])
-            setBarFillState(getBarFillPercent(leftThumbPos, newRightThumbValue))
-            setRightThumbPos(newRightThumbValue)
+        if (newRightThumbValue > value[0]) {
+            onChangeRange && onChangeRange([value[0], newRightThumbValue])
+            setBarFillState(getBarFillPercent(value[0], newRightThumbValue))
         }
         return
     }
 
     //fix for left thumb unable to move when both thumbs pressed to the right (right thumb is above by default)
-    const leftThumbClassName = leftThumbPos > max - 100 ? `${styles.thumb} ${styles.thumbZindex5}`
+    const leftThumbClassName = value[0] > max - 100 ? `${styles.thumb} ${styles.thumbZindex5}`
         : `${styles.thumb} ${styles.thumbZindex3}`
 
 
@@ -66,7 +60,7 @@ const SuperDoubleRange: React.FC<SuperDoubleRangePropsType> = (
                 type="range"
                 min={min}
                 max={max}
-                value={leftThumbPos}
+                value={value[0]}
                 onChange={leftThumbMoveHandler}
                 className={leftThumbClassName}
             />
@@ -74,7 +68,7 @@ const SuperDoubleRange: React.FC<SuperDoubleRangePropsType> = (
                 type="range"
                 min={min}
                 max={max}
-                value={rightThumbPos}
+                value={value[1]}
                 onChange={rightThumbMoveHandler}
                 className={`${styles.thumb} ${styles.thumbZindex4}`}
             />
